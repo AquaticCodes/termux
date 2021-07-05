28e37aebe772b39e892ef4a06b0439f551bfcfbb
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-07-05 19:31:39 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+echo Please Wait For 10 While We Start The Script!!
+
+sleep 10
+
+pkg update && pkg upgrade
+
+apt upgrade && apt update
+
+clear
+
+echo checking packages, click yes on anything you get
+
+sleep 10
+
+apt full-upgrade
+
+clear
+
+apt autoremove
+
+clear
